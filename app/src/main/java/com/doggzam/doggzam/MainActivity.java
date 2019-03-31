e9ac161c0f569a2bc9f3176d81bba91bf6545c9f
Aaron Crutchfield <aaroncrutchfield09@gmail.com>
2019-03-31 11:23:51 -0400

    feat: Upload images from the camera or gallery to recognize dog breed

--- app/src/main/java/com/doggzam/doggzam/MainActivity.java
+++ app/src/main/java/com/doggzam/doggzam/MainActivity.java
@@ -1,169 +1,294 @@
 package com.doggzam.doggzam;
 
-import android.app.Activity;
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.ml.vision.FirebaseVision;
-import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcode;
-import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetector;
-import com.google.firebase.ml.vision.common.FirebaseVisionImage;
-import com.google.firebase.ml.vision.label.FirebaseVisionLabel;
-import com.google.firebase.ml.vision.label.FirebaseVisionLabelDetector;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.gax.core.CredentialsProvider;
+import com.google.api.gax.core.FixedCredentialsProvider;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.auth.oauth2.ServiceAccountCredentials;
+import com.google.cloud.automl.v1beta1.AnnotationPayload;
+import com.google.cloud.automl.v1beta1.ExamplePayload;
+import com.google.cloud.automl.v1beta1.Image;
+import com.google.cloud.automl.v1beta1.ModelName;
+import com.google.cloud.automl.v1beta1.PredictResponse;
+import com.google.cloud.automl.v1beta1.PredictionServiceClient;
+import com.google.cloud.automl.v1beta1.PredictionServiceSettings;
+import com.google.protobuf.ByteString;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.InputStream;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import io.grpc.internal.IoUtils;
 
 public class MainActivity extends AppCompatActivity {
 
-    static final int REQUEST_IMAGE_CAPTURE = 1;
-    private FirebaseVisionBarcodeDetector detector;
-    private Uri photoURI;
-    TextView tvInfo;
+    // TODO: insert project info
+    private static final String COMPUTE_REGION = "us-central1"; // example: "us-central1";
+    private static final String PROJECT_ID = "dogg-zam-app"; // example: "workshop-vision";
+    private static final String MODEL_ID = "ICN6473335258590864622"; // example: "ICN293500301081240869";
+    private static final String SCORE_THRESHOLD = "0.5"; // example: "0.7";
+    private static final Integer JSON_RAW_RESOURCE_ID = R.raw.doggzamapp; // example: R.raw.workshopvision;
+
+    public static final String FILE_NAME = "temp.jpg";
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final int GALLERY_PERMISSIONS_REQUEST = 0;
+    private static final int GALLERY_IMAGE_REQUEST = 1;
+    public static final int CAMERA_PERMISSIONS_REQUEST = 2;
+    public static final int CAMERA_IMAGE_REQUEST = 3;
+
+    private TextView mImageDetails;
+    private ImageView mMainImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar =  findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        tvInfo = findViewById(R.id.tv_info);
-
-        FirebaseApp.initializeApp(this);
-
-        FloatingActionButton fab =  findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                dispatchTakePictureIntent();
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setMessage(R.string.dialog_select_prompt)
+                        .setPositiveButton(R.string.dialog_select_gallery, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                startGalleryChooser();
+                            }
+                        })
+                        .setNegativeButton(R.string.dialog_select_camera, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                startCamera();
+                            }
+                        });
+                builder.create().show();
             }
         });
+
+        mImageDetails = findViewById(R.id.image_details);
+        mMainImage = findViewById(R.id.main_image);
     }
 
-    private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            File photoFile = null;
-            try {
-                String imageFileName = "temp";
-                File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-                photoFile = File.createTempFile(
-                        imageFileName,  /* prefix */
-                        ".jpg",         /* suffix */
-                        storageDir      /* directory */
-                );
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            if (photoFile != null) {
-                photoURI = FileProvider.getUriForFile(MainActivity.this,
-                        "com.doggzam.fileprovider",
-                        photoFile);
-                Log.d("MainActivity", "onClick: " + photoURI);
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-            }
+    public void startGalleryChooser() {
+        if (PermissionUtils.requestPermission(this, GALLERY_PERMISSIONS_REQUEST, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            Intent intent = new Intent();
+            intent.setType("image/*");
+            intent.setAction(Intent.ACTION_GET_CONTENT);
+            startActivityForResult(Intent.createChooser(intent, "Select a photo"),
+                    GALLERY_IMAGE_REQUEST);
         }
+    }
+
+    public void startCamera() {
+        if (PermissionUtils.requestPermission(
+                this,
+                CAMERA_PERMISSIONS_REQUEST,
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.CAMERA)) {
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            Uri photoUri = FileProvider.getUriForFile(this, getApplicationContext().getPackageName() + ".provider", getCameraFile());
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            startActivityForResult(intent, CAMERA_IMAGE_REQUEST);
+        }
+    }
+
+    public File getCameraFile() {
+        File dir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        return new File(dir, FILE_NAME);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // if(data == null) return
+        // if(isGalleryRequest(requestcode, resultCode)
+        // if(isCameraRequst
+        if (requestCode == GALLERY_IMAGE_REQUEST && resultCode == RESULT_OK && data != null) {
+            uploadImage(data.getData());
+        } else if (requestCode == CAMERA_IMAGE_REQUEST && resultCode == RESULT_OK) {
+            Uri photoUri = FileProvider.getUriForFile(this, getApplicationContext().getPackageName() + ".provider", getCameraFile());
+            uploadImage(photoUri);
+        }
+    }
 
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case CAMERA_PERMISSIONS_REQUEST:
+                if (PermissionUtils.permissionGranted(requestCode, CAMERA_PERMISSIONS_REQUEST, grantResults)) {
+                    startCamera();
+                }
+                break;
+            case GALLERY_PERMISSIONS_REQUEST:
+                if (PermissionUtils.permissionGranted(requestCode, GALLERY_PERMISSIONS_REQUEST, grantResults)) {
+                    startGalleryChooser();
+                }
+                break;
+        }
+    }
 
-//            Uri photoUri = Uri.parse(data.getStringExtra("URI_EXTRA"));
-//
-//            imageView.setImageBitmap(photo);
-//            // Photo is returned. Density = 560
+    public void uploadImage(Uri imageUri) {
 
-//            Log.d(TAG, "onActivityResult: " + photoUri);
-            FirebaseVisionImage image = null;
+        if (imageUri != null) {
             try {
-                image = FirebaseVisionImage.fromFilePath(this, photoURI);
-                File file = new File(photoURI.toString());
-                file.delete();
-                Log.d("MainActivity", "onActivityResult: " + image.toString());
+                // scale the image to save on bandwidth
+                Bitmap bitmap = scaleBitmapDown(
+                        MediaStore.Images.Media.getBitmap(getContentResolver(), imageUri),
+                        1200);
+
+                mMainImage.setImageBitmap(bitmap);
+                findViewById(R.id.image_background).setVisibility(View.VISIBLE);
+
+                if (PROJECT_ID != null && COMPUTE_REGION != null && MODEL_ID != null && SCORE_THRESHOLD != null && imageUri != null) {
+                    PredictTask predictTask = new PredictTask();
+                    predictTask.execute(PROJECT_ID, COMPUTE_REGION, MODEL_ID, SCORE_THRESHOLD, imageUri.toString());
+                } else {
+                    mImageDetails.setText("ERROR: null prediction parameter.");
+                    Log.e("automl", "ERROR: null prediction parameter.");
+                }
+            } catch (IOException e) {
+                Log.d(TAG, "Image picking failed because " + e.getMessage());
+                Toast.makeText(this, R.string.image_picker_error, Toast.LENGTH_LONG).show();
+            }
+        } else {
+            Log.d(TAG, "Image picker gave us a null image.");
+            Toast.makeText(this, R.string.image_picker_error, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public Bitmap scaleBitmapDown(Bitmap bitmap, int maxDimension) {
+
+        int originalWidth = bitmap.getWidth();
+        int originalHeight = bitmap.getHeight();
+        int resizedWidth = maxDimension;
+        int resizedHeight = maxDimension;
+
+        if (originalHeight > originalWidth) {
+            resizedHeight = maxDimension;
+            resizedWidth = (int) (resizedHeight * (float) originalWidth / (float) originalHeight);
+        } else if (originalWidth > originalHeight) {
+            resizedWidth = maxDimension;
+            resizedHeight = (int) (resizedWidth * (float) originalHeight / (float) originalWidth);
+        } else if (originalHeight == originalWidth) {
+            resizedHeight = maxDimension;
+            resizedWidth = maxDimension;
+        }
+        return Bitmap.createScaledBitmap(bitmap, resizedWidth, resizedHeight, false);
+    }
+
+
+    public class PredictTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            // TODO: 3/4/2019 interface back to MainActivity to update imageDetails
+            mImageDetails.setText("Analysing image...");
+        }
+
+
+        @Override
+        protected String doInBackground(String... predictParams) {
+            try {
+                String projectId = predictParams[0];
+                String computeRegion = predictParams[1];
+                String modelId = predictParams[2];
+                String scoreThreshold = predictParams[3];
+                Uri uri = Uri.parse(predictParams[4]);
+
+                // TODO: 3/5/2019 Create getScopes method inside an interface from MainActivity
+                InputStream inputStream = getResources().openRawResource(JSON_RAW_RESOURCE_ID);
+
+                GoogleCredential credential;
+                credential = GoogleCredential.fromStream(inputStream);
+                Collection<String> scopes = Collections.singleton("https://www.googleapis.com/auth/cloud-platform");
+
+                if (credential.createScopedRequired()) {
+                    credential = credential.createScoped(scopes);
+                }
+
+                GoogleCredentials sac = ServiceAccountCredentials.newBuilder()
+                        .setPrivateKey(credential.getServiceAccountPrivateKey())
+                        .setPrivateKeyId(credential.getServiceAccountPrivateKeyId())
+                        .setClientEmail(credential.getServiceAccountId())
+                        .setScopes(scopes)
+                        // .setAccessToken(new AccessToken(credential.getAccessToken(), new LocalDate().plusYears(1).toDate()))
+                        .build();
+
+                // Latest generation Google libs, GoogleCredentials extends Credentials
+                CredentialsProvider cp = FixedCredentialsProvider.create(sac);
+                PredictionServiceSettings settings = PredictionServiceSettings.newBuilder().setCredentialsProvider(cp).build();
+
+                // Instantiate client for prediction service.
+                PredictionServiceClient predictionClient = PredictionServiceClient.create(settings);
+
+                // Get the full path of the model.
+                ModelName name = ModelName.of(projectId, computeRegion, modelId);
+
+                InputStream inputStreamImage = getContentResolver().openInputStream(uri);
+                byte[] bytes = IoUtils.toByteArray(inputStreamImage);
+                ByteString content = ByteString.copyFrom(bytes);
+                Image image = Image.newBuilder().setImageBytes(content).build();
+                ExamplePayload examplePayload = ExamplePayload.newBuilder().setImage(image).build();
+
+                // Additional parameters that can be provided for prediction e.g. Score Threshold
+                Map<String, String> params = new HashMap<>();
+                if (scoreThreshold != null) {
+                    params.put("score_threshold", scoreThreshold);
+                }
+                // Perform the AutoML Prediction request
+                PredictResponse response = predictionClient.predict(name, examplePayload, params);
+
+                String res = "";
+                res += "Prediction results:";
+                for (AnnotationPayload annotationPayload : response.getPayloadList()) {
+                    res += "\nPredicted class name: " + annotationPayload.getDisplayName();
+                    res += "\nPredicted class score: " + annotationPayload.getClassification().getScore();
+                }
+
+                Log.d("automl", res);
+                return res;
             } catch (IOException e) {
                 e.printStackTrace();
-                Log.d("MainActivity", "onActivityResult: IOException");
+                return e.toString();
             }
-
-            // Get an instance of FirebaseVision
-            FirebaseVisionLabelDetector detector = FirebaseVision.getInstance()
-                    .getVisionLabelDetector();
-
-
-            // TODO: 1/13/2019 Run recognition on image
-            Task<List<FirebaseVisionLabel>> result =
-                    detector.detectInImage(image)
-                            .addOnSuccessListener(
-                                    new OnSuccessListener<List<FirebaseVisionLabel>>() {
-                                        @Override
-                                        public void onSuccess(List<FirebaseVisionLabel> labels) {
-                                            // Task completed successfully
-                                            // ...
-                                            Log.d("MainActivity", "onSuccess: ");
-                                            for (FirebaseVisionLabel label: labels) {
-                                                String text = label.getConfidence() + ":\t" + label.getLabel() + "\n";
-                                                tvInfo.append(text);
-                                            }
-                                        }
-                                    })
-                            .addOnFailureListener(
-                                    new OnFailureListener() {
-                                        @Override
-                                        public void onFailure(@NonNull Exception e) {
-                                            // Task failed with an exception
-                                            // ...
-                                            Log.d("MainActivity", "onFailure: ");
-                                        }
-                                    });
-
-        }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
         }
 
-        return super.onOptionsItemSelected(item);
+        @Override
+        protected void onPostExecute(String result) {
+            mImageDetails.setText(result);
+        }
     }
 }
